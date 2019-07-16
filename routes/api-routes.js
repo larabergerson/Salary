ee61a93c8108d26e750933c778e359e52f0e9b83
lara bergerson <lara.bergerson@gmail.com>
2019-07-16 13:47:42 -0500

    "Added routes to players db"

---
+++ routes/api-routes.js
@@ -0,0 +1,35 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var connection = require("../config/connection.js");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+  // Get all players
+  app.get("/api/all", function(req, res) {
+    var dbQuery = "SELECT * FROM players";
+
+    connection.query(dbQuery, function(err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+  });
+
+  // Add a player
+  app.post("/api/new", function(req, res) {
+    console.log("Player Data: ");
+    console.log(req.body);
+
+    var dbQuery = "INSERT INTO players (player_name, player_email, wins, losses, draws) VALUES (?,?,?,?,?)";
+
+    connection.query(dbQuery, [req.body.player_name, req.body.player_email, req.body.wins, req.body.losses, req.body.draws], function(err, result) {
+      if (err) throw err;
+      console.log("Player Saved!");
+      res.end();
+    });
+  });
+};
