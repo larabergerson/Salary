0a3a370e5ce8b28bef69b1a393bd027b73716c4c
lara bergerson <lara.bergerson@gmail.com>
2019-07-18 14:28:47 -0500

    DELETED PHP, added passports
    Co-authored-by: hanksaam <hanksaam55@gmail.com>

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,21 +3,21 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.players.findAll({}).then(function(dbPlayers) {
-      res.render("index", {
-        msg: "Welcome!",
-        player: dbPlayers
-      });
-    });
+    // db.players.findAll({}).then(function(dbPlayers) {
+    //   res.render("index", {
+    //     msg: "Welcome!",
+    //     player: dbPlayers
+    //   });
+    // });
   });
 
   // Load example page and pass in an example by id
   app.get("/player/:id", function(req, res) {
-    db.Player.findOne({ where: { id: req.params.id } }).then(function(dbPlayers) {
-      res.render("player", {
-        player: dbPlayers
-      });
-    });
+    // db.Player.findOne({ where: { id: req.params.id } }).then(function(dbPlayers) {
+    //   res.render("player", {
+    //     player: dbPlayers
+    //   });
+    // });
   });
 
   // Render 404 page for any unmatched routes
