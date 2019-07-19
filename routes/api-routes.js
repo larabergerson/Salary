1c2a8da392ea514394708c8322830b406e84767e
hanksaam <50244027+hanksaam@users.noreply.github.com>
2019-07-19 14:03:55 -0500

    api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -9,27 +9,20 @@ var connection = require("../config/connection.js");
 // Routes
 // =============================================================
 module.exports = function(app) {
-  // Get all players
-  app.get("/api/all", function(req, res) {
-    var dbQuery = "SELECT * FROM scoreboard";
 
-    connection.query(dbQuery, function(err, result) {
+  app.post('/api/c/:correct/i/:incorrect', function(req, res) {
+    let query = `INSERT INTO scoreboard VALUES corrects, incorrects (?, ?)`;
+    connection.query(query, [req.params.corrects, req.params.incorrects], function (err, result) {
       if (err) throw err;
       res.json(result);
-    });
+    })
   });
 
-  // Add a player
-  app.post("/api/new", function(req, res) {
-    console.log("Player Data: ");
-    console.log(req.body);
-
-    var dbQuery = "INSERT INTO scorecard (corrects, incorrecrs) VALUES (?,?)";
-
-    connection.query(dbQuery, [req.body.corrects, req.body.incorrects], function(err, result) {
+  app.get('/api/scores', function(req, res) {
+    let query = "SELECT AVG(correct) AS average_correct FROM scoreboard";
+    connection.query(query, function(err, result) {
       if (err) throw err;
-      console.log("Player Saved!");
-      res.end();
-    });
+      res.json(result);
+    })
   });
-};
+}
